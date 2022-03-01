26e8531be4300fb089916731bd2f33a6a83abd5d
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-03-01 17:43:15 -0600

    Made formatURI static, changed variables to private

---
+++ views/template.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Website</title>
+    </head>
+    <body>
+        <?php
+            /** @var Page $page */
+            $page->getContent();
+        ?>
+    </body>
+</html>
