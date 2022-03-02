1fb6d9c82735c8191497de7623b99952845f5a57
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-03-01 19:13:23 -0600

    Added bootstrap

--- views/template.php
+++ views/template.php
@@ -1,12 +1,25 @@
+<?php
+/** @var Page $page */
+$p = $page; // Simply so my IDE stops complaining about an unknown variable
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Website</title>
-    </head>
-    <body>
-        <?php
-            /** @var Page $page */
-            $page->getContent();
-        ?>
-    </body>
+<head>
+    <title>Website</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <base href="<?php echo $page->getBaseURL() ?>">
+    <link rel="stylesheet" type="text/css" href="/static/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/static/css/page.css">
+</head>
+<body>
+
+    <?php
+    /** @var Page $page */
+    $page->getContent();
+    ?>
+    <script type="text/javascript" src="/static/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/d30a29881e.js" crossorigin="anonymous"></script>
+</body>
 </html>
