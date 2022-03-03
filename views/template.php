9421993671faf0e724977d57696694d971e3dcc0
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-03-03 02:05:28 -0600

    Added about link

--- views/template.php
+++ views/template.php
@@ -26,7 +26,10 @@ $p = $page; // Simply so my IDE stops complaining about an unknown variable
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="terminal-about-selector" aria-current="page" href="#">About</a>
                     </li>
                 </ul>
             </div>
