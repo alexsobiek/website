f1ceeeca67d94a7ad56b9269ddf09b7609d5c985
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-03-04 01:03:26 -0600

    Changed page title

--- views/template.php
+++ views/template.php
@@ -6,7 +6,7 @@ $p = $page; // Simply so my IDE stops complaining about an unknown variable
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Website</title>
+    <title>Alex Sobiek</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <base href="<?php echo $page->getBaseURL() ?>">
